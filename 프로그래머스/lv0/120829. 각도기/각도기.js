6140ccff2e991164dbb97a74e94469a841373b45
yeojin15 <115647373+yeojin15@users.noreply.github.com>
2022-12-01 16:02:44 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,6 +1,3 @@
 function solution(angle) {
-    if(angle === 180) return 4;
-    else if(angle > 90 && angle<180) return 3;
-    else if(angle === 90) return 2;
-    else return 1;
+    return angle===180 ? 4 : angle<180 && angle>90 ? 3 : angle===90 ? 2 : 1
 }
