a5e2449f3250494f853c970f4f9d7da00ca5ece1
yeojin15 <115647373+yeojin15@users.noreply.github.com>
2022-12-01 16:34:50 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -0,0 +1,13 @@
+function solution(n, k) {
+    // n 양꼬치 12000
+    // k 음료수 2000
+    // 만약 n>=10 이상이면 10단위로 k-1
+    // Math.floor(n/10) ?인분인지 구할수 있음 24
+    let by10 = Math.floor(n/10); // 2
+    let piece = n - by10*10; // 2인분 + 4개..
+    let paidDrink = k - by10; 
+    
+    let meat = (by10*10 + piece) * 12000
+    let drink = paidDrink * 2000
+    return meat + drink;
+}
