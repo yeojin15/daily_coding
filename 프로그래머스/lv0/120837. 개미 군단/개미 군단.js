ee60f89d6ae7e9516145fe0ac8d05d0966bb0bf1
yeojin15 <115647373+yeojin15@users.noreply.github.com>
2022-12-02 20:30:19 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,7 +1,7 @@
 function solution(hp) {
     // hp=29 5, 3, 1
-    let general = parseInt(hp/5) // general=5
-    let captain = parseInt((hp - general*5)/3)
+    let general = ~~(hp/5) // general=5
+    let captain = ~~((hp - general*5)/3)
     let soldier = hp - general*5 - captain*3
     
     return general+captain+soldier
