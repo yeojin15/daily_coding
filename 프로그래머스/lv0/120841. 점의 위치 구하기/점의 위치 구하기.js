a77d9867cebf140b891f126c45ab23e26e2c20e0
yeojin15 <115647373+yeojin15@users.noreply.github.com>
2022-12-03 19:58:48 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,11 @@
+function solution(dot) {
+    let x = dot[0];
+    let y = dot[1];
+    if(x > 0){
+        if(y > 0) return 1
+        else return 4
+    }else{
+        if(y>0) return 2
+        else return 3
+    }
+}
