03637d8700421781189a44cc728fceb27eb5a010
yeojin15 <115647373+yeojin15@users.noreply.github.com>
2022-12-26 13:06:51 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,8 +1,4 @@
 function solution(n) {
-    let xnum = 0;
-    for(let i=0; i<n; i++){
-        xnum++
-        while(xnum%3===0 || String(xnum).includes('3')) xnum++
-    }
-    return xnum;
+    var answer = 0;
+    return answer;
 }
