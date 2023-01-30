176418e7d51cda5ca4f35bb1f51ccb933bd88041
yeojin15 <115647373+yeojin15@users.noreply.github.com>
2023-01-30 19:35:14 +0900

    [unrated] Title: 귤 고르기, Time: 90.81 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,24 +1,27 @@
-function solution(k, tangerine) {
-    const obj = {}
-    // {크기:개수}
-    tangerine.forEach((el) => {
-        obj[el] =  ++obj[el] || 1 // 단축논리평가
-    })
-    
-    const arr=[]
-    for(let el in obj){
-        arr.push(obj[el])
-    }
-    let result = 0
-    arr.sort((a,b)=>b-a)
-    for(let i=0; i<arr.length;i++){
-        if(arr[i]-k>=0){
-            result = i+1
-            break;
+ function solution(k, tangerine) {
+      let cnt = 0;
+      let arr = tangerine.slice();
+      let obj = {};
+      for (e of arr) {
+        if (e in obj) {
+          obj[e]++;
+        } else {
+          obj[e] = 1;
         }
-        k -= arr[i]
+      }
+      // (2) 키값의 합으로 k만들기
+      // [1, 2, 2, 1, 2] -> [1, 1, 2, 2, 2] -> [2, 2, 2, 1, 1]
+      let newArr = Object.values(obj).sort((a, b)=>b-a)
+    
+      // (3) sort로 정렬해서 k와 처음부터 비교(큰수부터) 같거나 작아지는 순간 셈에 더함.
+      for (e of newArr) {
+
+          k -= e;
+          cnt += 1;
+        if (k <= 0) {
+          return cnt;
+        }
+      }
+
     }
     
-    return result
-
-}
