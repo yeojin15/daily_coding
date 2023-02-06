35f9c1726380f6423cba32236cd066a12674140d
yeojin15 <115647373+yeojin15@users.noreply.github.com>
2023-02-06 19:11:15 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -3,13 +3,13 @@ function solution(food) {
     // 1번째부터 반으로 나눠 내림가능하면 넣기
     let floor = food.map(item => Math.floor(item/2))
     let half = []
-    console.log(floor) // [1, 7, 1, 2] -> [ 0, 3, 0, 1 ]
+
     for(let i=1; i<floor.length; i++){
         for(let j=1; j<=floor[i]; j++){
             half.push(i)
         }
     }
-    let front = half.concat(0).concat(half.reverse())
+        let copy = half.slice()
+    let front = half.concat(0, copy.reverse())
     return front.join('')
-    
 }
