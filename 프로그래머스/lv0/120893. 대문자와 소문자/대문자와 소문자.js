87b320221649589ac8f29987699674e931cfc317
yeojin15 <115647373+yeojin15@users.noreply.github.com>
2023-04-13 16:12:35 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,13 +1,13 @@
 function solution(my_string) {
-    let answer = ''
-    for(let i of my_string){
-        if(i === i.toUpperCase()){
-            answer += i.toLowerCase();
-        }else{
-            answer += i.toUpperCase();
-        }
-    }
-    return answer;
-
+    // let answer = ''
+    // for(let i of my_string){
+    //     if(i === i.toUpperCase()){
+    //         answer += i.toLowerCase();
+    //     }else{
+    //         answer += i.toUpperCase();
+    //     }
+    // }
+    // return answer;
+    return my_string.split('').map(v=>v===v.toUpperCase()? v.toLowerCase() : v.toUpperCase()).join('')
 
 }
