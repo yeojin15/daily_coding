edd08704fa7ddb4c2c4f59464e140fd7f459d793
yeojin15 <115647373+yeojin15@users.noreply.github.com>
2023-04-17 10:45:18 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,10 +1,6 @@
 function solution(numbers) {
-    let sort = numbers.sort((a,b)=> a-b)
-    console.log(sort)
-    // 0,1 이 클지 arr.length-1, arr.length-2이 클지
-    let a = sort[0]*sort[1]
-    let b = sort[sort.length-1]*sort[sort.length-2]
-    return Math.max(a,b)
-    
 
+    
+    numbers.sort((a,b)=>a-b)
+    return Math.max(numbers[0]*numbers[1], numbers[numbers.length-1]*numbers[numbers.length-2])
 }
