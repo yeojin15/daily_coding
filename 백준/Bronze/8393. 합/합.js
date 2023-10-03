5af13c685e7eac6a20e355f0c67f33990cf6472c
yeojin15 <115647373+yeojin15@users.noreply.github.com>
2023-10-03 20:10:14 +0900

    [Bronze V] Title: 합, Time: 168 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.js
+++ 백준/Bronze/8393. 합/합.js
@@ -1,5 +1,9 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let n = Number(input[0]);
-console.log(n*(n+1)/2)
+let n = parseInt(input[0]);
+let sum = 0;
+for(let i=0; i<=n; ++i){
+    sum+=i;
+}
+console.log(sum)
